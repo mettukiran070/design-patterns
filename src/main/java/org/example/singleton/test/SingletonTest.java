24f63217b52a063a3362278cbcd7900808eab455
Mettu Kiran Kumar Reddy <mettu.kiran070@gmail.com>
2024-04-29 22:04:26 +0530

    Implementing the Singleton Design Pattern

--- src/main/java/org/example/singleton/test/SingletonTest.java
+++ src/main/java/org/example/singleton/test/SingletonTest.java
@@ -1,9 +1,31 @@
 package org.example.singleton.test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.example.singleton.def.EnumSingleton;
+import org.example.singleton.def.LazySingleton;
+
 public class SingletonTest {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+      throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    breakingSinglton();
 
+  }
+
+  private static void breakingSinglton()
+      throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    Constructor[] constructors = LazySingleton.class.getDeclaredConstructors();
+    Constructor constructor = constructors[0];
+    // making the constructor as public
+    constructor.setAccessible(true);
+    LazySingleton lazySingletonRef = (LazySingleton) constructor.newInstance();
+    LazySingleton lazySingleton = LazySingleton.getInstance();
+    System.out.println("Hash code of the object of Reflection Obj "+ lazySingletonRef.hashCode());
+    System.out.println("Hash code of the objcet of getInstance "+ lazySingleton.hashCode());
+
+    // Solution for this is Enum
+    EnumSingleton.INSTANCE.doSomething();
   }
 
 }
